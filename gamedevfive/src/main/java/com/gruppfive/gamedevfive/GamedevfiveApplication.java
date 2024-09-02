dc5b051e8f6f323a37c5ea7138f5c33a783e3220
mrsluggan <ericflyger@gmail.com>
2024-09-02 09:47:02 +0200

    first comit

---
+++ gamedevfive/src/main/java/com/gruppfive/gamedevfive/GamedevfiveApplication.java
@@ -0,0 +1,13 @@
+package com.gruppfive.gamedevfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamedevfiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamedevfiveApplication.class, args);
+	}
+
+}
