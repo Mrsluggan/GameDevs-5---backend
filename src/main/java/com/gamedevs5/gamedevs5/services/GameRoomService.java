c52ce5afd7157934837e0d86142fb18bacd2541f
mrsluggan <ericflyger@gmail.com>
2024-09-03 09:45:36 +0200

    BaiscCrud operations gameroomservice

--- src/main/java/com/gamedevs5/gamedevs5/services/GameRoomService.java
+++ src/main/java/com/gamedevs5/gamedevs5/services/GameRoomService.java
@@ -1,8 +1,29 @@
 package com.gamedevs5.gamedevs5.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
+
+import com.gamedevs5.gamedevs5.models.Gameroom.GameRoom;
 
 @Service
 public class GameRoomService {
+
+    private final MongoOperations mongoOperations;
+
+    @Autowired
+    public GameRoomService(MongoOperations mongoOperations) {
+        this.mongoOperations = mongoOperations;
+    }
+
+    public GameRoom getGameRoomById(String gameRoomId) {
+
+        return null;
+    }
+
+    public GameRoom createGameRoom(GameRoom newGameRoom) {
+
+        return null;
+    }
 
 }
