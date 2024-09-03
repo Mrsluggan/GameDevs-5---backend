ecd3e8a515c660fa4d3a2e0da19df2e4dad353eb
dennis <isacsson_89@hotmail.com>
2024-09-03 11:14:59 +0200

    Ordhantering

---
+++ src/main/java/com/gamedevs5/gamedevs5/controllers/WordController.java
@@ -0,0 +1,34 @@
+package com.gamedevs5.gamedevs5.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gamedevs5.gamedevs5.models.Word;
+import com.gamedevs5.gamedevs5.services.WordService;
+
+
+@RestController
+public class WordController {
+
+    @Autowired
+    WordService wordService;
+
+    @PostMapping("/createWord")
+    public Word createWord(@RequestBody Word word) {
+        return wordService.saveWord(word);
+    }
+
+    @GetMapping("/getWords")
+    public Object getWords() {
+        return wordService.getWords();
+    }
+
+    @GetMapping("getRandomWord")
+    public Word getRandomWord() {
+        return wordService.getRandomWord();
+    }
+    
+}
---
+++ src/main/java/com/gamedevs5/gamedevs5/services/WordService.java
@@ -0,0 +1,38 @@
+package com.gamedevs5.gamedevs5.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gamedevs5.gamedevs5.models.Word;
+import com.gamedevs5.gamedevs5.repositories.WordRepository;
+
+@Service
+public class WordService {
+
+    @Autowired
+    WordRepository wordRepository;
+
+    public Word saveWord(Word word) {
+        Word wordExists = wordRepository.findByWord(word.getWord());
+        if (wordExists != null) {
+            System.out.println("Ordet finns redan");
+            throw new IllegalArgumentException("Ordet finns redan");
+        }
+        if (word.getWord().length() < 1) {
+            System.out.println("Du glömde skriva in ett ord");
+            throw new IllegalArgumentException("Du glömde skriva in ett ord");
+        }
+        else {
+            return wordRepository.save(word);
+        }
+    }
+
+    public Object getWords() {
+        return wordRepository.findAll();
+    }
+
+    public Word getRandomWord() {
+        return wordRepository.findAll().get((int) (Math.random() * wordRepository.count()));
+    }
+    
+}
