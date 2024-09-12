08f604bb0016be3f87a8c91ffc5de09aa7a745fa
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-09-12 13:52:26 +0200

    Update WebSocketConfig.java

--- src/main/java/com/gamedevs5/gamedevs5/config/WebSocketConfig.java
+++ src/main/java/com/gamedevs5/gamedevs5/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-endpoint").setAllowedOrigins("https://monkfish-app-xpltr.ondigitalocean.app")
+        registry.addEndpoint("/ws-endpoint").setAllowedOrigins("https://urchin-app-dd7xw.ondigitalocean.app")
                 .withSockJS();
     }
 
