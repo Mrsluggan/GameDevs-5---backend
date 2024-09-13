c6461ea5e1ad838aca3c2be15713254b60c3046a
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-09-13 09:40:41 +0200

    crossorigin

--- src/main/java/com/gamedevs5/gamedevs5/config/WebSocketConfig.java
+++ src/main/java/com/gamedevs5/gamedevs5/config/WebSocketConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "*")
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
